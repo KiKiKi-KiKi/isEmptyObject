4ba86606ff50869ed79ebf097214fb27dc31b27f
KiKiKi <chaika.design@gmail.com>
2020-11-18 02:44:10 +0900

    Modify ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,13 @@ module.exports = {
     'newline-before-return': 'error',
     'no-console': 'warn',
     'no-var': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    // cf. https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-module-boundary-types.md
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'warn',
+      {
+        allowArgumentsExplicitlyTypedAsAny: true,
+      },
+    ],
   },
 };
