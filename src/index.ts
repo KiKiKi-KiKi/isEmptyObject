352cbe7b0967ad62b12250cf0e0cb7840042877c
KiKiKi <chaika.design@gmail.com>
2020-11-18 03:20:44 +0900

    :zap: Implement option of check an object has only own property.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-const isEmptyObject = (val?: any): boolean => {
-  return val !== null && typeof val === 'object' && val.constructor === Object && Object.keys(val).length === 0;
+const checkPrototypeProperty = (obj: typeof Object): boolean => {
+  for (const key in obj) {
+    if (!obj.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isEmptyObject = (val?: any, checkOwnProperty?: boolean): boolean => {
+  const result = val !== null && typeof val === 'object' && val.constructor === Object && Object.keys(val).length === 0;
+
+  if (!result || !checkOwnProperty) {
+    return result;
+  }
+
+  return checkPrototypeProperty(val);
 };
 
 export default isEmptyObject;
